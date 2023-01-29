d4c625c05827f9a29145f3928ed87b83dc2230b6
Dustin Goldade <dagoldade@gmail.com>
2023-01-29 10:27:58 +0100

    updated SelectActiveComponenet Btns

--- src/components/select_active_component/SelectActiveComponent.tsx
+++ src/components/select_active_component/SelectActiveComponent.tsx
@@ -2,23 +2,16 @@ import { Box, Container } from "@mui/material";
 import muiStyles from "../../locales/muiStyles.json";
 import { Link, useLocation } from "react-router-dom";
 import ElementButton from "../core_elements/ElementButton";
-import Text from "../../locales/en.json";
-import { useMemo } from "react";
+import { useState } from "react";
 import "./SelectActiveComponent.css";
-
+import { buttonsData } from "./buttonsData";
 const { PADDING_MED, PADDING_SM } = muiStyles;
-const {
-  SelecActiveComponentTab_Average_Of_Array,
-  SelecActiveComponentTab_BPM_Counter,
-  SelecActiveComponentTab_Change_Color_Theme,
-  SelecActiveComponentTab_Fetch_Img,
-} = Text;
 
 const SelectActiveComponent = () => {
-  const location = useLocation();
-
-  // Trigger rerender on changing url
-  useMemo(() => {}, [location.pathname]);
+  const currLocation = useLocation().pathname;
+  const [disabledButton, setDisabledButton] = useState(
+    currLocation === "/" ? "/arrayaverager" : currLocation
+  );
 
   return (
     <Container sx={{ maxWidth: "80%" }}>
@@ -30,33 +23,15 @@ const SelectActiveComponent = () => {
         sx={{ justifyContent: "space-around" }}
         className="header__a--remove_text_decoration"
       >
-        <Link to="/arrayaverager">
-          <ElementButton
-            label={SelecActiveComponentTab_Average_Of_Array}
-            disabled={
-              location.pathname === "/arrayaverager" ||
-              (location.pathname === "/" && true)
-            }
-          />
-        </Link>
-        <Link to="/bpmcounter">
-          <ElementButton
-            label={SelecActiveComponentTab_BPM_Counter}
-            disabled={location.pathname === "/bpmcounter" && true}
-          />
-        </Link>
-        <Link to="/themechanger">
-          <ElementButton
-            label={SelecActiveComponentTab_Change_Color_Theme}
-            disabled={location.pathname === "/themechanger" && true}
-          />
-        </Link>
-        <Link to="/imgfetcher">
-          <ElementButton
-            label={SelecActiveComponentTab_Fetch_Img}
-            disabled={location.pathname === "/imgfetcher" && true}
-          />
-        </Link>
+        {buttonsData.map((button, key) => (
+          <Link to={button.link} key={key}>
+            <ElementButton
+              label={button.label}
+              disabled={disabledButton === button.link}
+              onClick={() => setDisabledButton(button.link)}
+            />
+          </Link>
+        ))}
       </Box>
     </Container>
   );
---
+++ src/components/select_active_component/buttonsData.tsx
@@ -0,0 +1,25 @@
+import Text from "../../locales/en.json";
+const {
+  SelecActiveComponentTab_Average_Of_Array,
+  SelecActiveComponentTab_BPM_Counter,
+  SelecActiveComponentTab_Change_Color_Theme,
+  SelecActiveComponentTab_Fetch_Img,
+} = Text;
+export const buttonsData = [
+  {
+    link: "/arrayaverager",
+    label: SelecActiveComponentTab_Average_Of_Array,
+  },
+  {
+    link: "/bpmcounter",
+    label: SelecActiveComponentTab_BPM_Counter,
+  },
+  {
+    link: "/themechanger",
+    label: SelecActiveComponentTab_Change_Color_Theme,
+  },
+  {
+    link: "/imgfetcher",
+    label: SelecActiveComponentTab_Fetch_Img,
+  },
+];
